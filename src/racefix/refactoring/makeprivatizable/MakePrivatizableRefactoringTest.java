f5419aaf785f2541467805e812ed3c4311761a74
Caius Brindescu <caius.brindescu@gmail.com>
2012-07-09 20:12:15 +0300

    Basic test added

---
+++ src/racefix/refactoring/makeprivatizable/MakePrivatizableRefactoringTest.java
@@ -0,0 +1,20 @@
+package racefix.refactoring.makeprivatizable;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import racefix.refactoring.BasicTest;
+
+public class MakePrivatizableRefactoringTest extends BasicTest {
+	
+	@Before
+	public void setUp() {
+		super.setUp();
+	}
+	
+	@Test
+	public void testSimpleChange() {
+		
+	}
+
+}
