41c736691b9a3dcb8a44cbeb5005d667d1e4de8b
Caius Brindescu <caius.brindescu@gmail.com>
2012-07-09 21:28:44 +0300

    Implemented first test

--- src/racefix/refactoring/makeprivatizable/MakePrivatizableRefactoringTest.java
+++ src/racefix/refactoring/makeprivatizable/MakePrivatizableRefactoringTest.java
@@ -1,19 +1,33 @@
 package racefix.refactoring.makeprivatizable;
 
+import java.util.HashSet;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import racefix.refactoring.BasicTest;
+import racefix.refactoring.ClassChangeSet;
+import racefix.refactoring.RefactoringElement;
 
 public class MakePrivatizableRefactoringTest extends BasicTest {
-	
+
 	@Before
 	public void setUp() {
 		super.setUp();
 	}
-	
+
 	@Test
-	public void testInterfaceImplementsInclusion() {
+	public void testInterfaceImplementsInclusion() throws Exception {
+		ClassChangeSet change = new ClassChangeSet();
+		change.clazz = "in." + testName.getMethodName();
+		change.privatizedFields = new HashSet<String>();
+		change.threadLocal = new HashSet<String>();
+		
+		RefactoringElement element = new RefactoringElement(
+				decorateName("in.testInterfaceImplementsInclusion"),
+				new MakePrivatizableRefactoring(change));
+		element.apply();
+		assertFinalAs();
 	}
 
 }
